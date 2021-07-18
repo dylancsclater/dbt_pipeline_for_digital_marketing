f0a39f856393c5a1924b190ccd80546d7c76a260
dylancsclater@gmail.com <dylancsclater@gmail.com>
2021-07-18 21:59:37 +0000

    changed to materialize table instead of view

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -4,24 +4,13 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from dbt-tutorial-320221.dbt_dsclater.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt-tutorial-320221.dbt_dsclater.orders
+    select * from {{ ref('stg_orders')}}
 
 ),
 
